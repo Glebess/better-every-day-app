c706a399ae620c3bb5a9ef3dd5f0998d6aa2e493
Gleb Ermukov <yermukov@list.ru>
2026-05-14 23:37:24 +0200

    стили header , показ сегодняшего дня и дня неделт

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react'
 import styles from './Header.module.css'
 import HeaderButtonList from './HeaderButtonList'
 const Header = () => {
+	const date = new Date()
+	const days = [
+		'Воскресенье',
+		'Понедельник',
+		'Вторник',
+		'Среда',
+		'Четверг',
+		'Пятница',
+		'Суббота',
+	]
+
+	const [timeNow, setTimeNow] = useState({
+		dayName: days[date.getDay()],
+		day: date.getDate(),
+	})
 	return (
 		<header className={styles.header}>
-			<HeaderButtonList />
+			<HeaderButtonList timeNow={timeNow} />
 		</header>
 	)
 }
