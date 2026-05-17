9ecf13424c3b97054e404d78ee12f8787be0ca1f
Gleb Ermukov <yermukov@list.ru>
2026-05-17 21:10:10 +0200

    Получение сегодняшней даты ,форматировние даты

---
+++ src/utils/timeUtils.ts
@@ -0,0 +1,13 @@
+// Получение сегодняшней даты "год-месяц-день"
+export const getToday = (): string => {
+	return new Date().toDateString().split('T')[0]
+}
+
+// форматирование даты 2026-05-17 => 17 мая 2026
+export const formatDate = (date: string): string => {
+	return new Date(date).toLocaleDateString('ru-RU', {
+		day: 'numeric', // число
+		month: 'long', // месяц
+		year: 'numeric', // год полностью
+	})
+}
