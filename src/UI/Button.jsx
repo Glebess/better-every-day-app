de9fde8e72473fa05b7c3a97464ec696e6bd8432
Gleb Ermukov <yermukov@list.ru>
2026-05-28 22:13:53 +0200

    анимация настроек

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,11 +1,12 @@
 const Button = (props) => {
-	const { children, onClick, className, disabled = false, key } = props
+	const { children, onClick, className, disabled = false, key, style } = props
 	return (
 		<button
 			onClick={onClick}
 			className={className}
 			disabled={disabled}
 			key={key}
+			style={style}
 		>
 			{children}
 		</button>
--- src/modules/HabitsWidget/SettingsHabitsWidget/SettingsHabitsWidget.jsx
+++ src/modules/HabitsWidget/SettingsHabitsWidget/SettingsHabitsWidget.jsx
@@ -1,30 +1,29 @@
 import { Info, List, Settings, Trash } from 'lucide-react'
 import Button from '../../../UI/Button'
 import styles from './SettingsHabitsWidget.module.css'
+
 const SettingsHabitsWidget = () => {
+	const buttons = [
+		{ icon: Settings, text: 'Настройки', delay: '0s' },
+		{ icon: Info, text: 'Детали', delay: '0.1s' },
+		{ icon: List, text: 'Показать списком', delay: '0.2s' },
+		{ icon: Trash, text: 'Убрать', delay: '0.3s' },
+	]
+
 	return (
 		<div className={styles.settings_container}>
-			<Button className={styles.button_settings}>
-				<p>Настройки</p>
-				<Settings />
-			</Button>
-
-			<Button className={styles.button_settings}>
-				<p>Детали</p>
-				<Info />
-			</Button>
-
-			<Button className={styles.button_settings}>
-				<p>Показать списком</p>
-				<List />
-			</Button>
-			{/* <Columns3 /> */}
-
-			<Button className={styles.button_settings}>
-				<p>Убрать</p>
-				<Trash />
-			</Button>
+			{buttons.map((button, index) => (
+				<Button
+					key={index}
+					className={styles.button_settings}
+					style={{ '--delay': button.delay }}
+				>
+					<p>{button.text}</p>
+					<button.icon />
+				</Button>
+			))}
 		</div>
 	)
 }
+
 export default SettingsHabitsWidget
